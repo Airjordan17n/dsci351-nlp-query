d8506b458f087218ec19cc557cdf903d9bdea5e0
ppoddar0101 <preyashipoddar@gmail.com>
2025-04-23 07:34:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ Return ONLY a valid **MySQL** query using SQL syntax — no explanation, no mark
         
 # --- Execute SQL query ---
 def execute_mysql_query(query):
-    #ssh_host = 'ec2-18-225-255-15.us-east-2.compute.amazonaws.com'
+    #ssh_host = 'ec2-3-144-253-89.us-east-2.compute.amazonaws.com'
     #ssh_user = 'ubuntu'
     #ssh_key = 'dsci351.pem'  # Upload this file via Colab sidebar if needed
 
@@ -189,7 +189,7 @@ def execute_mysql_query(query):
     #        port=tunnel.local_bind_port
     #    )
     connection = pymysql.connect(
-        host="ec2-18-225-255-15.us-east-2.compute.amazonaws.com",  # public IP or hostname
+        host="ec2-3-144-253-89.us-east-2.compute.amazonaws.com",  # public IP or hostname
         user="root",
         password="Dsci351",
         database="transactions_db",
@@ -208,7 +208,7 @@ def execute_mysql_query(query):
 
 # --- Execute MongoDB query ---
 def execute_mongo_query(query, datasets_list):
-    mongo_uri = "mongodb://ec2-18-225-255-15.us-east-2.compute.amazonaws.com:27017"
+    mongo_uri = "mongodb://ec2-3-144-253-89.us-east-2.compute.amazonaws.com:27017"
     try:
         client = MongoClient(mongo_uri, serverSelectionTimeoutMS=5000)
         db = client["ecommerce"]
