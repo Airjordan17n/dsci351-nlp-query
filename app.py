53d79a11b55815d0aa49437f45d1f3d2f8fbbb26
ppoddar0101 <preyashipoddar@gmail.com>
2025-04-23 07:39:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -166,35 +166,35 @@ Return ONLY a valid **MySQL** query using SQL syntax — no explanation, no mark
         
 # --- Execute SQL query ---
 def execute_mysql_query(query):
-    ssh_host = 'ec2-3-144-253-89.us-east-2.compute.amazonaws.com'
-    ssh_user = 'ubuntu'
-    ssh_key = 'dsci351.pem'  # Upload this file via Colab sidebar if needed
+    #ssh_host = 'ec2-3-144-253-89.us-east-2.compute.amazonaws.com'
+    #ssh_user = 'ubuntu'
+    #ssh_key = 'dsci351.pem'  # Upload this file via Colab sidebar if needed
 
-    mysql_host = 'localhost'
-    mysql_user = 'root'
-    mysql_password = 'Dsci351'
-    mysql_db = 'transactions_db'
+    #mysql_host = 'localhost'
+    #mysql_user = 'root'
+    #mysql_password = 'Dsci351'
+    #mysql_db = 'transactions_db'
 
-    with SSHTunnelForwarder(
-        (ssh_host, 22),
-        ssh_username=ssh_user,
-        ssh_pkey=ssh_key,
-        remote_bind_address=('127.0.0.1', 3306)
-    ) as tunnel:
-        connection = pymysql.connect(
-            host=mysql_host,
-            user=mysql_user,
-            password=mysql_password,
-            database=mysql_db,
-            port=tunnel.local_bind_port
-        )
-    #connection = pymysql.connect(
-    #    host="ec2-3-144-253-89.us-east-2.compute.amazonaws.com",  # public IP or hostname
-    #    user="root",
-    #    password="Dsci351",
-    #    database="transactions_db",
-    #    port=3306
-    #)
+    #with SSHTunnelForwarder(
+    #    (ssh_host, 22),
+    #    ssh_username=ssh_user,
+    #    ssh_pkey=ssh_key,
+    #    remote_bind_address=('127.0.0.1', 3306)
+    #) as tunnel:
+    #    connection = pymysql.connect(
+    #        host=mysql_host,
+    #        user=mysql_user,
+    #        password=mysql_password,
+    #        database=mysql_db,
+    #        port=tunnel.local_bind_port
+    #    )
+    connection = pymysql.connect(
+        host="ec2-3-144-253-89.us-east-2.compute.amazonaws.com",  # public IP or hostname
+        user="root",
+        password="Dsci351",
+        database="transactions_db",
+        port=3306
+    )
 
     with connection.cursor() as cursor:
         cursor.execute(query)
